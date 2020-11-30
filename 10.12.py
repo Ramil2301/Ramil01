9475f146e620cb8b08f3eb9a5fd92fa2ab1b7bde
Ramil2301 <71386291+Ramil2301@users.noreply.github.com>
2020-11-30 17:34:48 +0300

    Delete 10.12.py

--- 10.12.py
+++
@@ -1,9 +0,0 @@
-from collections import Counter
-
-a = [[1, 2, 3, 5, 4],
-     [1, 2, 2, 5, 4],
-     [1, 5, 5, 5, 4],
-     [1, 1, 1, 1, 1],
-     [1, 1, 1, 0, 0]]
-maxs = [Counter(x).most_common(1)[0][1] for x in a]
-print(maxs.index(max(maxs)) + 1)
